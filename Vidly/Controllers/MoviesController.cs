73b1dc8e12188e70604ff7695616cd7fea822189
NateThompson114 <NateThompson114@gmail.com>
2018-04-17 10:47:29 -0400

    Added Movies to views along with actions.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,25 +1,37 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre);
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        [Route(@"Movies/id/{id}")]
+        public ActionResult Movie(int id)
         {
-            return new List<Movie>
-            {
-                new Movie{Id = 1,Name = "Gone With The Wind."},
-                new Movie{Id = 2, Name = "Star Wars"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
+
+            return View(movie);
         }
     }
 }
