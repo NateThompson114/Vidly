b13a797c636af07561c20b9002b5f709e64d7cca
NateThompson114 <NateThompson114@gmail.com>
2018-04-30 16:31:51 -0400

    Added rental model, dto, and controller.

---
+++ Vidly2/Controllers/API/RentalController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly2.Dtos;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers.API
+{
+    public class RentalController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult RentlMovies(RentalDto rental)
+        {
+            var cust = _context.Customers.SingleOrDefault(m => m.Id == rental.CustomerId);
+
+            if (cust == null)
+                return NotFound();
+
+            if (!rental.MovieIds.Any())
+                return NotFound();
+
+            return Ok();
+        }
+    }
+}
---
+++ Vidly2/Models/Rental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        //public int CustomerId { get; set; }
+
+        //public int MovieId { get; set; }
+
+        public DateTime DateRented { get; set; }
+
+        public DateTime DateReturned { get; set; }
+    }
+}
